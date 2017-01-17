cf3183c5c05fed9bab1110e730983ce9cd8bff0c
jjj883831 <huyitgm@gmail.com>
2017-01-17 14:48:22 +0700

    api
    api

---
+++ Models/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCtestCore.Models
+{
+    public class HelloWorld : Controller
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime year { get; set; }
+        
+        public HelloWorld (int _id,string _name,DateTime _year)
+        {
+            id = _id;
+            name = _name;
+            year = _year;
+        }
+    }
+}
